57a8ca6bb542011b9e1c00e034f3de86b9d9bff4
Pablo Soares <slpkvol3@gmail.com>
2018-08-26 17:08:45 -0300

    Change data storage facility to MapComponent

--- Rimworld_Prospector/WorldDataStore.cs
+++ Rimworld_Prospector/WorldDataStore.cs
@@ -1,32 +1,40 @@
-﻿using System.Collections.Generic;
+﻿﻿using System.Collections.Generic;
 using HugsLib.Utils;
-using Verse;
+ using RimWorld.Planet;
+ using Verse;
 
 namespace Rimworld_Prospector
 {
     // ReSharper disable once ClassNeverInstantiated.Global
-    public class WorldDataStore : UtilityWorldObject
+    public class WorldDataStore : MapComponent
     {
+        public WorldDataStore (Map map) : base(map)
+        {
+            MinedOre = new List<Thing>();
+            GiveJobDoneTracker = new GiveJobDoneTracker();
+            PawnPackAnimalTracker = new Dictionary<string, Pawn>();
+        }
+
         /**
          * The helper for tracking Jobs' doneness
          */
         public GiveJobDoneTracker GiveJobDoneTracker;
+
         /**
          * The list of mined ore 
          */
         public List<Thing> MinedOre;
 
-        public override void SpawnSetup()
-        {
-            base.SpawnSetup();
-            MinedOre = new List<Thing>();
-            GiveJobDoneTracker = new GiveJobDoneTracker();
-        }
+        /**
+         * Pack animal
+         */
+        public Dictionary<string, Pawn> PawnPackAnimalTracker;
 
         public override void ExposeData() {
             base.ExposeData();
             Scribe_Values.Look(ref GiveJobDoneTracker, "GiveJobDoneTracker");
             Scribe_Values.Look(ref MinedOre, "MinedOre");
+            Scribe_Values.Look(ref PawnPackAnimalTracker, "PawnPackAnimalTracker");
         }
     }
 }
